dbc9b6b16464a2f3256d96ddf93d7a9c7cb51952
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-23 14:55:39 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create ([
+    {product: "ビションフリーゼ"},
+    {product: "キャバリア・キングチャールズ・スパニエル"},
+    {product: "シベリアン・ハスキー"}
+])
