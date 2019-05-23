a67fb8f39d2e0a4ddd11d35f03d0d02483e278a4
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-05-23 16:30:27 +0900

    import 3 packages

--- crud_front/src/Components/MainContainer.jsx
+++ crud_front/src/Components/MainContainer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import ProductsContainer from './ProductsContainer';
 import FormContainer from './FormContainer';
 
